54cc1629b9bc61fa50585306f9d6bc06a26d8a8b
KarbonatErolcoded <144907554+KarbonatErolcoded@users.noreply.github.com>
2024-02-07 14:53:20 +0100

    Sign up now says sign up

--- bytewars-master/src/components/views/LoginPage/signUp.js
+++ bytewars-master/src/components/views/LoginPage/signUp.js
@@ -4,12 +4,12 @@ import './login.css';
 function SignUp() {
   return (
     <div className="login">
-      <h1>Login</h1>
+      <h1>Sign Up</h1>
       <form method="post">
         <input type="text" name="u" placeholder="Username" required="required" />
         <input type="password" name="p" placeholder="Password" required="required" />
         <input type="password-confirm" name="pc" placeholder="Confirm Password" required="required" />
-        <button type="submit" className="btn login-btn">Login</button>
+        <button type="submit" className="btn login-btn">Sign Up</button>
         {/* Plain text link for Sign Up */}
         <p className="signup-text">
           Already have an account? <a href="/auth" className="signup-link">login here!</a>
