3c29def9c959c6375d7622e68d5f1694a672c57e
KarbonatErolcoded <144907554+KarbonatErolcoded@users.noreply.github.com>
2024-02-09 08:18:40 +0100

    Navbar changed and Homescreen card removed

--- bytewars-master/src/components/Navbar/Navbar.js
+++ bytewars-master/src/components/Navbar/Navbar.js
@@ -1,50 +1,26 @@
 import React from 'react';
-import './Navbar.css';
- 
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css'; // Make sure to create and import the CSS file
+
 function Navbar() {
-  // Function to check if a link is active
-  const isActive = (path) => {
-    return window.location.pathname === path;
-  };
- 
+  const location = useLocation(); // Hook to get the current path
+
   return (
-    <nav>
-      <div className="navbar-container">
-        <a href="/" className="navbar-logo">
-          <span className="logo-byte">Byte</span><span className="logo-wars">Wars</span>
-        </a>
-        <ul className="navbar-coding">
-        <li className="navbar-item no-bullet">
-            <a href="/coding" className={`navbar-coding-links ${isActive('/') ? 'active' : ''}`}>
-              &lt;Coding Area/&gt;
-            </a>
-          </li>
-          </ul>
-        <ul className="navbar-menu">
-          <li className="navbar-item">
-            <a href="/" className={`navbar-links ${isActive('/') ? 'active' : ''}`}>
-              Home
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="/practice" className={`navbar-links ${isActive('/practice') ? 'active' : ''}`}>
-              Practice
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="#services" className={`navbar-links ${isActive('#services') ? 'active' : ''}`}>
-              Services
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="/auth" className={`navbar-links ${isActive('/auth') ? 'active' : ''}`}>
-              Login
-            </a>
-          </li>
-        </ul>
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
+        <span className="logo-byte">Byte</span><span className="logo-wars">Wars</span>
+      </Link>
+
+      <div className="navbar-center">
+        <Link to="/coding" className="coding-button">Coding</Link>
+      </div>
+
+      <div className="navbar-right">
+        <Link to="/leaderboard" className={`right-button ${location.pathname === '/leaderboard' ? 'active' : ''}`}>Leaderboard</Link>
+        <Link to="/auth" className={`right-button ${location.pathname === '/auth' ? 'active' : ''}`}>Login</Link>
       </div>
     </nav>
   );
 }
- 
+
 export default Navbar;
--- bytewars-master/src/components/views/Home/Home.js
+++ bytewars-master/src/components/views/Home/Home.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './Home.css';
 import WelcomeAnimation from '../../WelcomeAnimation/WelcomeAnimation';
-import Card from '../../Card/Card';
+
 
 function Home() {
     return (
       <div className="home-container">
         <div className="content-container">
           <WelcomeAnimation />
-          <div className="cards-container">
-            <Card name="Practice Area" path="/practice" />
-            <Card name="Compete" path="/path-for-card-2" />
-            <Card name="Single Player" path="/path-for-card-3" />
-            {}
-          </div>
+          
         </div>
       </div>
     );
